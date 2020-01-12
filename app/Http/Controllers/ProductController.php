f516cfe01d6ab13e59f2acb737a179fedc8d80fa
LutenkoMikhail <lamprologus2011@gmail.com>
2020-01-12 17:46:04 +0200

    Add controller and request

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthorCreateRequest;
+use App\Http\Requests\ProductCreateRequest;
+use App\Product;
+use Illuminate\Support\Facades\Config;
+
+class ProductController extends Controller
+{
+    public function __construct()
+    {
+        $this->paginate = Config::get('constants.paginate.paginate_author_10');
+    }
+
+    public function index()
+    {
+        $products = Product::paginate($this->paginate);
+        return view('welcome', [
+            'products' => $products
+        ]);
+    }
+
+    public function show(Product $product)
+    {
+        return view('sorry',
+            [
+                'nameClass' => __CLASS__,
+                'nameMethod' => __METHOD__
+            ]
+        );
+    }
+
+    public function create()
+    {
+        return view('sorry',
+            [
+                'nameClass' => __CLASS__,
+                'nameMethod' => __METHOD__
+            ]
+        );
+    }
+
+    public function store(ProductCreateRequest $request)
+    {
+        return view('sorry',
+            [
+                'nameClass' => __CLASS__,
+                'nameMethod' => __METHOD__
+            ]
+        );
+    }
+
+    public function edit(Product $product)
+    {
+        return view('sorry',
+            [
+                'nameClass' => __CLASS__,
+                'nameMethod' => __METHOD__
+            ]
+        );
+    }
+
+    public function update(ProductCreateRequest $request, Product $product)
+    {
+
+        return view('sorry',
+            [
+                'nameClass' => __CLASS__,
+                'nameMethod' => __METHOD__
+            ]
+        );
+    }
+
+    public function delete(Product $product)
+    {
+        return view('sorry',
+            [
+                'nameClass' => __CLASS__,
+                'nameMethod' => __METHOD__
+            ]
+        );
+    }
+}
